001e791e596a69c3925d9ac1a5377c6aa063c0eb
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-09 06:23:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Laba_6_Alecseev_ISP_232_ISRPO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
